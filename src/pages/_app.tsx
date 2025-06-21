f003cce2ee6d17b1ab8b3d3f2894361feb429693
arunanksharan <arunank.sharan@gmail.com>
2025-06-22 04:04:42 +0530

    [feat]: add title and description to metadata

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,6 +6,12 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        {/* Default title that can be overridden by individual pages */}
+        <title>Kuzushi Labs</title>
+        <meta
+          name="description"
+          content="Here at Kuzushi Labs we engineer AI native solutions. We are committed to helping you transform your ideas into innovative and inspiring world-class products."
+        />
         {/* Add the favicon here */}
         <link rel="icon" href="/favicon.png" />
         {/* Or use PNG */}
